77864a49b53cb2211d07dbfb8ab1ba20b1f73db7
nod3zer0 <game1men@gmail.com>
2024-02-01 01:52:02 +0100

    added migrations for tags and properties
    TODO: add costrainsts. They dont work at the moment for some reason.

---
+++ migrations/03_init_tag.php
@@ -0,0 +1,21 @@
+<?php
+class InitTag extends Migration
+{
+    public function up()
+    {
+        $db = DBManager::get();
+        $query = "CREATE TABLE IF NOT EXISTS mp_tag (
+            id CHAR(32) CHARACTER SET latin1 COLLATE
+            latin1_bin NOT NULL,
+            name TEXT NOT NULL,
+            PRIMARY KEY (id)
+            )";
+        $db->exec($query);
+    }
+
+    public function down()
+    {
+        // drop new tables
+        DBManager::get()->exec("DROP TABLE IF EXISTS mp_tag");
+    }
+}
--- migrations/04_init_tagdemand.php
+++ migrations/04_init_tagdemand.php
@@ -1,18 +1,16 @@
 <?php
-class InitProperty extends Migration
+class InitTagdemand extends Migration
 {
     public function up()
     {
         $db = DBManager::get();
-        $query = "CREATE TABLE IF NOT EXISTS mp_property (
+        $query = "CREATE TABLE IF NOT EXISTS mp_tag_demand (
             id CHAR(32) CHARACTER SET latin1 COLLATE
             latin1_bin NOT NULL,
-            name TEXT NOT NULL,
-            type TINYINT(2) NOT NULL DEFAULT 1,
-            matching BOOL NOT NULL DEFAULT 1,
             demand_id CHAR(32) CHARACTER SET latin1 COLLATE
             latin1_bin NOT NULL,
-            value TEXT,
+            tag_id CHAR(32) CHARACTER SET latin1 COLLATE
+            latin1_bin NOT NULL,
             PRIMARY KEY (id)
             )";
         $db->exec($query);
@@ -21,6 +19,6 @@ class InitProperty extends Migration
     public function down()
     {
         // drop new tables
-        DBManager::get()->exec("DROP TABLE IF EXISTS mp_property");
+        DBManager::get()->exec("DROP TABLE IF EXISTS mp_tag_demand");
     }
 }
