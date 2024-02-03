3f7b4655ca6206833887f8c3cb9b9b86e2942914
nod3zer0 <game1men@gmail.com>
2024-02-03 22:05:40 +0100

    added db searching, first draft
    - cannot search yet, just access to databases on predefined query

--- controllers/search.php
+++ controllers/search.php
@@ -9,5 +9,11 @@ class SearchController extends \Marketplace\Controller
     {
         Navigation::activateItem('marketplace_root/marketplace_search');
         PageLayout::setTitle('Search');
+        $db = DBManager::get();
+        $this->st = $db->fetchAll("SELECT * FROM mp_demand", []);
+    }
+
+    public function search_action()
+    {
     }
 }
--- views/search/index.php
+++ views/search/index.php
@@ -1,3 +1,29 @@
-<table class="default sortable-table">
+<?
 
-</table>
+use Studip\Button; ?>
+<form class="default collapsable" action="<?= $controller->link_for('search/search', $demand_obj->id) ?>" method="post">
+    <?= CSRFProtection::tokenTag() ?>
+    <fieldset data-open="bd_basicsettings">
+        <div>
+            <label class="required">
+                search
+            </label>
+            <input name="search-query" required value="">
+        </div>
+    </fieldset>
+
+    <div>
+        <? foreach ($st as $row) : ?>
+            <div>
+                <div>
+                    Title
+                </div>
+                <div><?= $row['title'] ?></div>
+            </div>
+        <? endforeach; ?>
+    </div>
+
+    <footer data-dialog-button>
+        <?= Button::create('Submit') ?>
+    </footer>
+</form>
