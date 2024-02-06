d8d446150dd2ad8a6ba503a96536cc51c1e9a9e1
nod3zer0 <game1men@gmail.com>
2024-02-06 01:39:16 +0100

    VUEJS, custom_property
    added VUEJS to config page
    added custom_property table

--- views/config/index.php
+++ views/config/index.php
@@ -1,1 +1,28 @@
-<?
+<div id="app">
+    <ul>
+        <li v-for="(item, index) in properties" :key="index">
+            <input v-model="properties[index]">
+            <button @click="deleteItem(index)">Delete</button>
+        </li>
+    </ul>
+    <button @click="addItem">Add property</button>
+</div>
+
+<script>
+    STUDIP.Vue.load().then(({Vue, createApp, eventBus, store}) =>  {
+        new Vue({
+        el: '#app',
+        data: {
+            properties: []
+        },
+        methods: {
+            addItem: function() {
+                this.properties.push('');
+            },
+            deleteItem: function(index) {
+                this.properties.splice(index, 1);
+            }
+        }
+    });
+    });
+</script>
